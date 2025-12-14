342f4c72d19f32faca4efa00d5104f6a75977ec1
suraj <surajskadam7@gmail.com>
2025-12-14 16:52:00 +0530

    new

--- main.go
+++ main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	calculate(10, 20)
+}
+
+func calculate(a int, b int)  {
+	fmt.Println("Addition of", a, "and", b, "is", adding(a, b))
+	fmt.Println("Subtraction of", a, "and", b, "is", subtracting(a, b))
+}
+
+func adding(a int, b int) int {
+	return a + b
+}
+
+func subtracting(a int, b int) int {
+	return a - b
 }
